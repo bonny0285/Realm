b76db0873ce80df8290288838fe2229cd0cea632
Massimiliano <massimiliano@MacBook-Pro-di-Massimiliano.local>
2019-05-04 16:08:16 +0100

    show last run

--- Treads/Controller/BeginRunVC.swift
+++ Treads/Controller/BeginRunVC.swift
@@ -13,12 +13,19 @@ class BeginRunVC: LocationVC {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var lastRunBgView: UIView!
+    @IBOutlet weak var lastRunStack: UIStackView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationAuthStatus()
         mapView.delegate = self
+        
        // Run.getAllRuns()
         //print("here are my runs: \(Run.getAllRuns())")
     }
@@ -27,13 +34,38 @@ class BeginRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
+    
+    func getLastRun(){
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunStack.isHidden = true
+            lastRunBgView.isHidden = true
+            lastRunCloseBtn.isHidden = true
+            return
+        }
+        lastRunStack.isHidden = false
+        lastRunBgView.isHidden = false
+        lastRunCloseBtn.isHidden = false
+        paceLbl.text = lastRun.pace.formatTimeDurationToString()
+        distanceLbl.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
+        durationLbl.text = lastRun.duration.formatTimeDurationToString()
+    }
+    
+    
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
     }
+    
+    
+    @IBAction func LastRunCloseBtnPressed(_ sender: Any) {
+        lastRunStack.isHidden = true
+        lastRunBgView.isHidden = true
+        lastRunCloseBtn.isHidden = true    }
+    
     
 }
 
